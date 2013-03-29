b2f2dac21094ce173363a116512ed961398d7a4f
Lars Greiss <kufikugel@googlemail.com>
2013-03-29 12:50:53 +0100

    Nozomi: initial changes for slim
    thanks RaymanFX for the base
    
    Change-Id: Ibbe27464815961a82909438f315d00f38d45262c

--- slim.mk
+++ slim.mk
@@ -16,20 +16,29 @@
 
 $(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit common GSM stuff.
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# Inherit common Phone stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := nozomi
+
+PRODUCT_NAME := slim_nozomi
+PRODUCT_DEVICE := nozomi
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Xperia S
+PRODUCT_MANUFACTURER := Sony
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
-
-PRODUCT_NAME := cm_nozomi
-PRODUCT_DEVICE := nozomi
